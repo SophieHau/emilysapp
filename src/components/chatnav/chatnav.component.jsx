e514fd8dd5b99e6d92e06d45a03f263bd4b02e73
sophie <s.hautcoeur@gmail.com>
2020-05-14 11:13:47 +0300

    fixed chat name length in chatnav

--- src/components/chatnav/chatnav.component.jsx
+++ src/components/chatnav/chatnav.component.jsx
@@ -19,14 +19,28 @@ export class ChatNav extends React.Component {
     }
 
     render() {
-        const { chatName } = this.state
+        let { chatName } = this.state
+        if (chatName.length > 30) {
+            chatName = chatName.slice(0, 25)
+            return (
+                <nav className="dt w-90 center pt2 mb3 ml2 bb b--black-05">
+                <div className="dtc mr3 fl v-mid f6">
+                    <Link to="/"><img src={backIcon} className="mt1 br-100 v-mid mr2" style={{width: '25px', height: '25px'}} alt="arrow"/></Link> 
+                    <img src="http://tachyons.io/img/logo.jpg" className="mt1 h2 w2 br-100 v-mid" alt="profile"/>
+                </div>
+                <div className="dtc v-mid fl">
+                    <p className="dark-gray f6 pa0 mr3">{chatName}...</p>
+                </div>
+            </nav>
+            )
+        }
+
         return (
             <nav className="dt w-90 center pt2 mb3 ml2 bb b--black-05">
                 <div className="dtc mr3 fl v-mid f6">
                     <Link to="/"><img src={backIcon} className="mt1 br-100 v-mid mr2" style={{width: '25px', height: '25px'}} alt="arrow"/></Link> 
                     <img src="http://tachyons.io/img/logo.jpg" className="mt1 h2 w2 br-100 v-mid" alt="profile"/>
                 </div>
-                    
                 <div className="dtc v-mid fl">
                     <p className="dark-gray f6 pa0 mr3">{chatName}</p>
                 </div>
